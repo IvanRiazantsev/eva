fe0deb92c4a2c69e8b04db7742198899e9e11f40
Ivan Ryazantsev <ivan.riazantsev@gmail.com>
2020-11-19 21:26:59 +0200

    email, phone, power image fix

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,6 +1,6 @@
 module HomeHelper
-  def tel_to(phone_number)
-    phone_number = number_to_phone phone_number, country_code: 38
+  def tel_to(phone_number:, country_code:)
+    phone_number = number_to_phone phone_number, country_code: country_code
     link_to phone_number, "tel:#{phone_number}"
   end
 end
